35ac73684ec55f13ac7b66769083fcc5c5f0e181
Alex <alexandre.tokka@gmail.com>
2023-11-25 12:41:06 +0100

    Original code

---
+++ rename_files_with_regex.bash
@@ -0,0 +1,14 @@
+#REGEX 
+regex='S([0-9])_E([0-9]{1,2}).([[:alpha:]]{2,4})' 
+
+#LOOP ON EVERY FILES OF CURRENT FOLDER AND SUBFOLDERS
+for file in $(find -name \*.*); 
+do
+	echo $file
+	if [[ $file =~ $regex ]]
+	then
+		mkdir "./season ${BASH_REMATCH[1]}"
+		mv $file "./season ${BASH_REMATCH[1]}/Attack on titan {tbm-0000}_s0${BASH_REMATCH[1]}e${BASH_REMATCH[2]}.${BASH_REMATCH[3]}"; 
+	fi
+done 
+
